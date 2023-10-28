55a755f23e6ef1ad8e83dc33ec862295294c176d
dongjilo <121302078+dongjilo@users.noreply.github.com>
2023-10-29 01:43:42 +0800

    fixed typo

--- db/dbconnection.php
+++ db/dbconnection.php
@@ -9,14 +9,14 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn -> connect_error) {
     die("Connection failed: " . $conn -> connect_error);
-}
+} else {
 //    echo "<div class='container'>
 //      <div class='alert alert-success alert-dismissible fade show' role='alert'>
 //        <div>Database successfully connected</div>
 //        <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
 //      </div>
 //      </div>";
-
+}
 
 
 
