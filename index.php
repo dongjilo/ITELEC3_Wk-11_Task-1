2a3e5e9f6f2fa2bea8ede87c85c80fa462200e19
dongjilo <121302078+dongjilo@users.noreply.github.com>
2023-10-29 19:35:37 +0800

    updated delete icon

--- index.php
+++ index.php
@@ -98,8 +98,8 @@
                     echo "<td>$username</td>";
                     echo "<td>$password</td>";
                     echo "<td class='text-center'><a href='operation/delete.php?id=$id' class='btn btn-danger btn-sm' onclick='return confirm(`Are you sure you want to delete this row?`)'>
-                            <svg xmlns='http://www.w3.org/2000/svg' width='12' height='12' fill='currentColor' class='bi bi-pencil-square flex-shrink-0 me-2' viewBox='0 0 16 16'>
-                              <path d='M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z'/>
+                            <svg xmlns='http://www.w3.org/2000/svg' width='12' height='12' fill='currentColor' class='bi bi-trash-fill flex-shrink-0 me-2' viewBox='0 0 16 16'>
+                              <path d='M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z'/>
                             </svg>Delete</a>
                             <a href='edit.php?id=$id' class='btn btn-warning btn-sm'>
                             <svg xmlns='http://www.w3.org/2000/svg' width='12' height='12' fill='currentColor' class='bi bi-x-lg flex-shrink-0 me-2' viewBox='0 0 16 16'>
